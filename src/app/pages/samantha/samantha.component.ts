77d9ec74b449f9a661453a54ba50d356b9ea67a5
Samantha Fernández <samanthaangelesfp@msmk.university>
2025-02-20 13:16:13 +0100

    commit calculadora

--- src/app/pages/samantha/samantha.component.ts
+++ src/app/pages/samantha/samantha.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-samantha',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [CalculadoraComponent],
   templateUrl: './samantha.component.html',
   styleUrl: './samantha.component.css'
 })
 export class SamanthaComponent {
+
+
 
 }
